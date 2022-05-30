c101660c65bbd7a7776a0ccc66b14a858fc52c83
Muhammad Hasnain zaib <muhammadhasnain0334@gmail.com>
2022-05-30 18:12:02 +0500

    delete line

--- cluster/cv/script.sh
+++ cluster/cv/script.sh
@@ -7,5 +7,3 @@ yq eval -i '.spec.values.deployment.image.tag ="'${tag}'"' "${file}"
 git add "${file}"
 git commit -m "${msg}"
 git push 
-
-#yq eval -i '.spec.values.deployment.image.tag="0.0.7"' helm-release.yaml
