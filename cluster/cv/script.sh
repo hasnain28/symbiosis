b75d2196a27a6d49aa724bcac11b69abf1faf678
Muhammad Hasnain zaib <muhammadhasnain0334@gmail.com>
2022-05-30 18:05:24 +0500

    add

--- cluster/cv/script.sh
+++ cluster/cv/script.sh
@@ -4,7 +4,7 @@ file="$2"
 msg="$3"
 # -i tag is used to implement changes in file
 yq eval -i '.spec.values.deployment.image.tag ="'${tag}'"' "${file}"
-git add file
+git add "${file}"
 git commit -m "${msg}"
 git push 
 
