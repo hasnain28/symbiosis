b08545660b0335dd626b1e92248dec5bc981c0d5
Muhammad Hasnain zaib <muhammadhasnain0334@gmail.com>
2022-05-30 18:02:00 +0500

    add

---
+++ cluster/cv/script.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+tag="$1"
+file="$2"
+msg="$3"
+# -i tag is used to implement changes in file
+yq eval -i '.spec.values.deployment.image.tag ="'${tag}'"' "${file}"
+git add file
+git commit -m "${msg}"
+git push 
+
+#yq eval -i '.spec.values.deployment.image.tag="0.0.7"' helm-release.yaml
